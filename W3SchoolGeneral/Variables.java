2f5c61f9bd9bcffd8cc85462ce8a586172719bb0
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2022-04-23 23:23:21 +0500

    feat

---
+++ W3SchoolGeneral/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    // In Java, there are different types of variables, for example:
+    // String - stores text, such as "Hello". String values are surrounded by double
+    // quotes
+    // int - stores integers (whole numbers), without decimals, such as 123 or -123
+    // float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+    // char - stores single characters, such as 'a' or 'B'. Char values are
+    // surrounded by single quotes
+    // boolean - stores values with two states: true or false
+    public static void main(String[] args) {
+        int qarza_of_pakistan = 18000;
+        String qarza = qarza_of_pakistan + " USD Million";
+        System.out.println("The qarza in pakistan is equal to : " + qarza);
+    }
+}
